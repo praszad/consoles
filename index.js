14e4cc0c0afbfcdd60ed3a0a07650228c660371f
silverabbot <prasadbabu.s@contus.in>
2019-07-01 19:45:00 +0530

    Types Added

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-const c = require('consoles');
+const C = function(...S) {
+    let len = S.length;
+    if (len <= 1) {
+        return console.log(S[0]);
+    }
+    if (len >= 1) {
+        var v = [];
+        for (i = 0; i < len; i++) {
+            v.push(S[i])
+        }
+        return console.log(v);
+    }
+}
+const c = C;
+const consoles = C;
 
-c(43);
+const T = function(...S) {
+    let len = S.length;
+    if (len <= 1) {
+        return console.log(typeof S[0]);
+    }
+    if (len >= 1) {
+        var v = [];
+        for (i = 0; i < len; i++) {
+            v.push(typeof S[i])
+        }
+        return console.log(v);
+    }
+}
+const t = T;
+const types = T;
+const type = T;
+module.exports = { C, c, consoles, t, type, types, T };
